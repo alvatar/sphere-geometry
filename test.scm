0fbefb741b11dbf200f7c3a01a6b8ac425957501
Álvaro Castro-Castilla <alvaro@castrocastilla.com>
2010-05-13 18:04:26 +0200

    Cleaner tests

--- test.scm
+++ test.scm
@@ -9,7 +9,18 @@
 (import math/algebra)
 (import kernel)
 
+(define-syntax test-equal-vect2
+  (syntax-rules ()
+   ((_ name expr result)
+    (test-assert name (vect2:=? expr result)))))
+
+;-------------------------------------------------------------------------------
 (test-begin "geometry")
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Construction
+;-------------------------------------------------------------------------------
 
 (test-equal "segment->line horizontal line"
   (segment->line (make-segment (make-point 2.2 -2.0)
@@ -21,22 +32,26 @@
                                (make-point 11.0 1.0)))
   (make-line 0.0 1.0 -1.0))
 
-(test-assert "intersection:line-line 2"
-  (vect2:=?
-    (intersection:line-line
-      (segment->line (make-segment (make-point 0.0 0.0)
-                                   (make-point 2.0 2.0)))
-      (segment->line (make-segment (make-point 0.0 2.0)
-                                   (make-point 2.0 0.0))))
-    (make-point 1.0 1.0)))
+;-------------------------------------------------------------------------------
+; Intersections
+;-------------------------------------------------------------------------------
 
-(test-assert "intersection:line-line 2"
-  (vect2:=?
-    (intersection:line-line
-      (segment->line (make-segment (make-point 0.0 0.0)
-                                   (make-point 2.0 2.0)))
-      (segment->line (make-segment (make-point -2.0 0.0)
-                                   (make-point 0.0 -2.0))))
-    (make-point -1.0 -1.0)))
+(test-equal-vect2 "intersection:line-line 2"
+  (intersection:line-line
+    (segment->line (make-segment (make-point 0.0 0.0)
+                                 (make-point 2.0 2.0)))
+    (segment->line (make-segment (make-point 0.0 2.0)
+                                 (make-point 2.0 0.0))))
+  (make-point 1.0 1.0))
 
+(test-equal-vect2 "intersection:line-line 2"
+  (intersection:line-line
+    (segment->line (make-segment (make-point 0.0 0.0)
+                                 (make-point 2.0 2.0)))
+    (segment->line (make-segment (make-point -2.0 0.0)
+                                 (make-point 0.0 -2.0))))
+  (make-point -1.0 -1.0))
+
+;-------------------------------------------------------------------------------
 (test-end "geometry")
+;-------------------------------------------------------------------------------
