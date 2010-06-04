aeefe13e23c05c89dd26764f1525e48cac2e7be2
Álvaro Castro-Castilla <alvaro@castrocastilla.com>
2010-06-05 00:15:47 +0200

    Moved part of algorithm to generation + helpers

--- generation.scm
+++ generation.scm
@@ -5,25 +5,40 @@
 ;;; Geometry generation procedures
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(import (std srfi/1))
+(import ../core/syntax)
+(import ../core/functional)
 (import ../math/exact-algebra)
-
+(import ../math/inexact-algebra)
 (import kernel)
 
 ;;; Return a random point that is inside a given pseq
 
-(define (pseq:make-random-point-inside point-list)
-  (define (try origin delta)
-    (let ((p (make-point (+ (point-x origin)
-                            (* (point-x delta) (random-real)))
-                         (+ (point-y origin)
-                            (* (point-y delta) (random-real))))))
-      (if (pseq:point-inside? point-list p)
-          p
-        (try origin delta))))
-  (let* ((bounding-box (pseq->bounding-box point-list))
-         (bb-left-corner (bounding-box-lefttop bounding-box))
-         (bb-right-corner (bounding-box-rightbottom bounding-box)))
-    (try
-      bb-left-corner
-      (vect2:-vect2 bb-right-corner
-                    bb-left-corner))))
+(define (generate.random-point-inside pseq)
+  (define (gen a b)
+    (aif p (curry pseq:point-inside? pseq)  
+         (make-point (random-real/range (point-x a) (point-x b))
+                     (random-real/range (point-y a) (point-y b)))
+         p (gen origin delta)))
+  (let ((bounding-box (pseq->bounding-box pseq)))
+    (gen
+     (bounding-box-lefttop bounding-box)
+     (bounding-box-rightbottom bounding-box))))
+
+;;; Return a random point that is inside a given pseq
+
+(define (generate.random-points/separation&boundaries N pseq p-dist b-dist)
+                                        ; TODO: OPTIMIZE, A way would be dividing the space
+  (define (respects-distances? p plis)
+    (every (lambda (p-in-plis) (< p-dist (~distance.point-point p p-in-plis))) plis))
+  (define (respects-boundaries? p)
+    (< b-dist (~distance.point-pseq p pseq)))
+  (define (gen n plis)
+    (aif p (lambda (p) (and (respects-distances? p plis)
+                       (respects-boundaries? p)))
+         (generate.random-point-inside pseq)
+         (if (>= n N)
+             (cons p plis)
+             (gen (add1 n) (cons p plis)))
+         (gen n plis)))
+  (gen 0 '()))
